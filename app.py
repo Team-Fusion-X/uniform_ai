92caf84edb327ebb237be8fda72eae9994c9a4e3
mingd00 <rkdalswl403@naver.com>
2023-12-04 20:24:44 +0900

    [feat] Create Model

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from keras.models import load_model
+import numpy as np
+
+model = load_model('model/best_model.h5')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    #데이터 받을 빈 리스트 생성
+    data_list = []  
+
+    #사용자가 입력한 값 불러오기
+    data_list = request.get_json()['data_list']
+
+    #데이터 형태 맞춰주기
+    input_query = np.array([data_list], dtype=np.float32)
+
+	#모델과 비교
+    result = model.predict(input_query)[0]
+
+    #json 형식으로 return
+    return jsonify({'합격 확률':str(result)})
+
+if __name__ == '__main__':
+    # Run Flask app
+    app.run(debug=True, port=8080)
